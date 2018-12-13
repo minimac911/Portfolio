bd12f86cdc49f94c9f6ad5c8ffb17cb38f21d086
minimac911 <thomasmcalpine77@gmail.com>
2018-12-13 22:58:11 +0200

    Changing idea to space themed

---
+++ src/space.js
@@ -0,0 +1,24 @@
+// var star = document.getElementsByClassName("pulse");
+// var counter = 0;
+// var blnUp = true;
+
+// setInterval(() => { 
+//     let w = document.getElementById("pulse").style.width;
+//     let h = document.getElementById("pulse").style.height;
+//     if(blnUp){
+//         counter++;
+//         if(counter===10){
+//             blnUp = false;
+//         }
+//     }else{
+//         counter--;
+//         if(counter===0){
+//             blnUp = true;
+//         }
+//     }
+//     w += counter;
+//     h += counter;
+//     document.getElementById("pulse").style.width = w+"px";
+//     document.getElementById("pulse").style.height = h+"px";
+//     counter++;
+// }, 1000);
