972f5ae4e2c933284f8491d4877c02efe1a68cc2
minimac911 <thomasmcalpine77@gmail.com>
2018-12-14 19:32:43 +0200

    added type writer text

--- src/space.js
+++ src/space.js
@@ -1,54 +1,55 @@
-function createStaicStar(){
+function createStaicStar() {
     var space = document.getElementById("space");
     var star = document.createElement("div");
 
     space.appendChild(star);
     star.className = "pulse";
 
-    star.style.marginLeft = randNumGen()+"%";
+    star.style.marginLeft = randNumGen() + "%";
 
-    let mTop = randNumGen()-75;
-    if(mTop<0){
-        mTop = randNumRange(0,20);
+    let mTop = randNumGen() - 25;
+    if (mTop < 0) {
+        mTop = randNumRange(0, 20);
     }
-    star.style.marginTop = mTop+"%";
+    star.style.marginTop = mTop + "%";
 
     var colour = getRandomColor();
     star.style.backgroundColor = colour;
 
-    let widthAndHeigth = randNumRange(2,6)
+    let widthAndHeigth = randNumRange(2, 6)
     star.style.width = widthAndHeigth + "px";
     star.style.height = widthAndHeigth + "px";
 
-    star.style.animation = "pulse "+randNumRange(1,15)+"s infinite";
+    star.style.animation = "pulse " + randNumRange(1, 15) + "s infinite";
 }
 
 //----------RANFOM GENERATOR FUNCTIONS------------------------------------------
-function randNumRange(min, max){
+function randNumRange(min, max) {
     return Math.floor((Math.random() * max) + min);
 }
 
-function randNumGen(){
+function randNumGen() {
     let num = Math.floor((Math.random() * 99) + 1);
     return num;
 }
 
-function randAmountStars(){
+function randAmountStars() {
     let num = Math.floor((Math.random() * 50) + 30);
     return num;
-} 
+}
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
 }
 //-----------END OF RANDOM FUNCTIONS   -----------------------------------------
 
-let numStars = randNumRange(25,screen.width*0.02);
-for(let i = 0; i < numStars; i++){
+
+let numStars = randNumRange(100, screen.width * 0.05);
+for (let i = 0; i < numStars; i++) {
     createStaicStar();
 }
