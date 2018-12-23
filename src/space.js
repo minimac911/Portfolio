a8b93aa18a9c144bc9bd95bde9383095f5c07845
minimac911 <thomasmcalpine77@gmail.com>
2018-12-24 01:00:40 +0200

    Added new hamburger with semi working options

--- src/space.js
+++ src/space.js
@@ -16,7 +16,7 @@ function createStaicStar() {
     var colour = getRandomColor();
     star.style.backgroundColor = colour;
 
-    let widthAndHeigth = randNumRange(1.5, 5)
+    let widthAndHeigth = randNumRange(2, 5)
     star.style.width = widthAndHeigth + "px";
     star.style.height = widthAndHeigth + "px";
     if(randNumGen()<=25){
@@ -52,46 +52,46 @@ function getRandomColor() {
 }
 //-----------END OF RANDOM FUNCTIONS   -----------------------------------------
 
-var planet = document.getElementById("circle");
-var option = [];
-option.push(document.getElementById("planet_git"));
-option.push(document.getElementById("planet_about")); 
-option.push(document.getElementById("planet_contact"));  
-var blnClicked = false;
-planet.onclick = function(){
-    var counter = 0;
-    var speed = 0.3;
-    if(!blnClicked){//GOING FORWARD
-        planet.style.left = 20+"px";
-        planet.style.animationPlayState = "paused";
-        option.forEach((x)=>{
-            x.style.left = 45+"px";
-        });
-        //animation for the option planets
-        option.forEach((x)=>{
-            x.style.transition = "left "+speed+"s 0."+counter+"s";
-            x.style.webkitTransition = "left "+speed+"s 0."+counter+"s";
-            counter++;
-        });
-    }else{//GOING BACK
-        planet.style.left = -60+"px";
-        planet.style.animationPlayState = "running";
-        option.forEach((x)=>{
-            x.style.left = -200+"px";
-        });
-        //animation for the option planets\
-        counter = option.length
-        option.forEach((x)=>{
-            x.style.transition = "left "+speed+"s 0."+counter+"s";
-            x.style.webkitTransition = "left "+speed+"s 0."+counter+"s";
-            counter--;
-        });
-    }
-    counter = option.length-counter;
-    planet.style.transition = "left "+speed+"s 0."+counter+"s";
-    planet.style.webkitTransition = "left "+speed+"s 0."+counter+"s";
-    blnClicked = !blnClicked;
-};
+// var planet = document.getElementById("circle");
+// var option = [];
+// option.push(document.getElementById("planet_git"));
+// option.push(document.getElementById("planet_about")); 
+// option.push(document.getElementById("planet_contact"));  
+// var blnClicked = false;
+// planet.onclick = function(){
+//     var counter = 0;
+//     var speed = 0.3;
+//     if(!blnClicked){//GOING FORWARD
+//         planet.style.left = 20+"px";
+//         planet.style.animationPlayState = "paused";
+//         option.forEach((x)=>{
+//             x.style.left = 45+"px";
+//         });
+//         //animation for the option planets
+//         option.forEach((x)=>{
+//             x.style.transition = "left "+speed+"s 0."+counter+"s";
+//             x.style.webkitTransition = "left "+speed+"s 0."+counter+"s";
+//             counter++;
+//         });
+//     }else{//GOING BACK
+//         planet.style.left = -60+"px";
+//         planet.style.animationPlayState = "running";
+//         option.forEach((x)=>{
+//             x.style.left = -200+"px";
+//         });
+//         //animation for the option planets\
+//         counter = option.length
+//         option.forEach((x)=>{
+//             x.style.transition = "left "+speed+"s 0."+counter+"s";
+//             x.style.webkitTransition = "left "+speed+"s 0."+counter+"s";
+//             counter--;
+//         });
+//     }
+//     counter = option.length-counter;
+//     planet.style.transition = "left "+speed+"s 0."+counter+"s";
+//     planet.style.webkitTransition = "left "+speed+"s 0."+counter+"s";
+//     blnClicked = !blnClicked;
+// };
 
 // var shootingStars = [];
 // shootingStars[0] = document.getElementById("p-1");
@@ -105,12 +105,25 @@ planet.onclick = function(){
 //     time_1 = randNumRange(1,20);
 // }, time_1*1000);
 
+var hamburger = document.querySelector('.hamburger');
+var opt = document.querySelector(".options");
+var name = document.getElementById("name-heading");
+hamburger.onclick = function(){
+    hamburger.classList.toggle("is-active");
+    if(hamburger.classList.contains("is-active")){
+        opt.style.visibility = "visible";
+    }else{
+        opt.style.visibility = "hidden";
+    }
+}
+
 function create(){
     let numStars = randNumRange(100, screen.width * 0.07);
     for (let i = 0; i < numStars; i++) {
         createStaicStar();
     }
 }
+
 create();
 
 
