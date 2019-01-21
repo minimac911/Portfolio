55e682ab86577f648ade8676ed8b8aace57335cc
minimac911 <thomasmcalpine77@gmail.com>
2019-01-21 17:43:42 +0200

    -

--- js/hamburger.js
+++ js/hamburger.js
@@ -2,13 +2,23 @@ $(document).ready(function(){
 	$('#nav-icon2').click(function(){
         $(this).toggleClass('open');
         if( $( this ).hasClass( "open" )){
-            $(".options").animate({
+            $(".options-list").animate({
                 left : '0px'
             });
         }else{
-            $(".options").animate({
+            $(".options-list").animate({
                 left : '-200px'
             });
         }
 	});
+});
+
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+	
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	} /* speed */ );
 });
