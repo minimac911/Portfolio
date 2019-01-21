6a447019f9985b0cffa820b001569066e03d590c
minimac911 <thomasmcalpine77@gmail.com>
2019-01-21 13:31:03 +0200

    starting to make it responsive to smaller laptop

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,16 +1,14 @@
-var ham = document.getElementById("hamburger");
-var lineOne = document.getElementById("line-one");
-var lineTwo = document.getElementById("line-two");
-var lineThree = document.getElementById("line-three");
-var clicked = false;
-
-ham.addEventListener("click", function (e) {
-    clicked = !clicked;
-    if (clicked) {
-        lineOne.style.animation = "hamburger-clicked-one-active 0.7s forwards";
-        lineOne.style.webkitAnimation = "hamburger-clicked-one-active 0.7s forwards";
-    } else {
-        lineOne.style.animation = "hamburger-clicked-one-disable 0.7s forwards";
-        lineOne.style.webkitAnimation = "hamburger-clicked-one-disable 0.7s forwards";
-    }
-}, false);
+$(document).ready(function(){
+	$('#nav-icon2').click(function(){
+        $(this).toggleClass('open');
+        if( $( this ).hasClass( "open" )){
+            $(".options").animate({
+                left : '0px'
+            });
+        }else{
+            $(".options").animate({
+                left : '-200px'
+            });
+        }
+	});
+});
